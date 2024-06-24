77dac9d378c18225725c397569afe74d111548c4
Valentin Kiesche <valentin.kiesche@study.hs-duesseldorf.de>
2024-06-24 20:05:36 +0200

    Nav Mesh Baking during runtime works

--- Assets/Scripts/NavMeshBaker.cs
+++ Assets/Scripts/NavMeshBaker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
@@ -6,53 +7,24 @@ using Meta.XR.MRUtilityKit;
 using UnityEngine.AI;
 
 public class NavMeshBaker : MonoBehaviour
-{
+{ 
+    private NavMeshSurface navMeshSurface;
+    void Awake()
+    {
+       navMeshSurface = GetComponent<NavMeshSurface>();
+    }
 
     void Start()
     {
-        int settingsCount = NavMesh.GetSettingsCount();
-
-        for (int i = 0; i < settingsCount; i++)
-        {
-            NavMeshBuildSettings settings = NavMesh.GetSettingsByIndex(i);
-            if (settings.agentTypeID == NavMesh.GetSettingsByIndex(i).agentTypeID)
-            {
-                Debug.Log("Index für 'cat' Agententyp gefunden: " + i);
-                /*break;*/
-            }
-        }
+        BakeNavMesh();
     }
-
-    public void InitializeNavMesh()
+    [ContextMenu("BakeNavMesh")]
+    public void BakeNavMesh()
     {
-        /*FindObjForNavMesh("")*/
-        GameObject floorAR = FindObjectForNavMesh("FLOOR");
-        // Füge der Ebene eine NavMeshSurface-Komponente hinzu
-        var navMeshSurface = floorAR.AddComponent<NavMeshSurface>();
-        // Baken der NavMesh zur Laufzeit
-        SetUpNavMesh(navMeshSurface);
-        BakeNavMesh(navMeshSurface);
-    }
-
-    void BakeNavMesh(NavMeshSurface navMeshSurface)
-    {
-        
-        // Starte das Baken der NavMesh zur Laufzeit
         navMeshSurface.BuildNavMesh();
     }
-
     void SetUpNavMesh(NavMeshSurface navMeshSurface)
     {
         navMeshSurface.agentTypeID = NavMesh.GetSettingsByIndex(1).agentTypeID;
-    }
-
-    GameObject FindObjectForNavMesh(string gameObjName)
-    {
-        string fullEffectMeshPath = $"{gameObjName}/{gameObjName}_EffectMesh";
-        Debug.Log(fullEffectMeshPath);
-        MRUKRoom mrukComponent = FindObjectOfType<MRUKRoom>();
-        GameObject mrukObject = mrukComponent.gameObject;
-        var navMeshObj = mrukObject.transform.Find(fullEffectMeshPath).gameObject;
-        return navMeshObj;
     }
 }
