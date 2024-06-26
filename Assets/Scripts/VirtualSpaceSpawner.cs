53b23e6c4411991cc16671e2ed985c41a938b929
Valentin Kiesche <valentin.kiesche@study.hs-duesseldorf.de>
2024-06-26 17:08:30 +0200

    Environment scene is loaded during runtime but cant be repositioned

--- Assets/Scripts/VirtualSpaceSpawner.cs
+++ Assets/Scripts/VirtualSpaceSpawner.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 
 public class VirtualSpaceSpawner : MonoBehaviour
 {
-    [FormerlySerializedAs("prefabToPlace")] public GameObject outsideEnvironment;
+    private static bool sceneLoaded = false;
     public GameObject insideEnvironment;
-    
+
+    private GameObject outsideEnvironment;
     // Start is called before the first frame update
     void Start()
     {
-        if(outsideEnvironment != null)
-            PlacePrefab(outsideEnvironment);
+        if (!sceneLoaded)
+        {
+            sceneLoaded = true;
+            StartCoroutine(LoadEnvironmentScene());
+        }
+
         if(insideEnvironment != null)
             PlacePrefab(insideEnvironment);
     }
@@ -29,5 +35,38 @@ public class VirtualSpaceSpawner : MonoBehaviour
         thisPosition.y += prefabToPlace.transform.position.y;
         Quaternion thisRotation = Quaternion.Euler(0.0f, transform.rotation.eulerAngles.y, 0.0f);
         Instantiate(prefabToPlace, thisPosition, thisRotation);
+    }
+
+    private void RepositionGameObject(GameObject gameObjectToTransform)
+    {
+        Vector3 thisPosition = transform.position;
+        thisPosition.y = 0.0f;
+        thisPosition.y += gameObjectToTransform.transform.position.y;
+        Quaternion thisRotation = Quaternion.Euler(0.0f, transform.rotation.eulerAngles.y, 0.0f);
+        gameObjectToTransform.transform.position = thisPosition;
+        gameObjectToTransform.transform.rotation = thisRotation;
+    }
+
+    IEnumerator LoadEnvironmentScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+        
+        // Warte, bis das Laden abgeschlossen ist
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        Scene loadedScene = SceneManager.GetSceneByBuildIndex(1);
+        GameObject[] rootObjects = loadedScene.GetRootGameObjects();
+        foreach (GameObject obj in rootObjects)
+        {
+            if (obj.CompareTag("Environment"))
+            {
+                Debug.Log("Hey Umgebung gefunden");
+                outsideEnvironment = obj;
+                RepositionGameObject(outsideEnvironment);
+                break;
+            }
+        }
     }
 }
